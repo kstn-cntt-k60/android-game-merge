bd1f3563bbd7a6e96d0923ddfc803b8fcda475b6
DESKTOP-Q3VI6FE\thang <tabaothang97@gmail.com>
2017-12-08 15:23:59 +0700

    Add disactive

--- app/src/main/java/kstn/game/view/thang/adapter/MultiAdapter.java
+++ app/src/main/java/kstn/game/view/thang/adapter/MultiAdapter.java
@@ -1,6 +1,7 @@
 package kstn.game.view.thang.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,12 +21,19 @@ import kstn.game.logic.state.multiplayer.Player;
 public class MultiAdapter extends BaseAdapter {
     private ArrayList<Player> data;
     private Context context;
+    private ArrayList<ImageView> dataImg = new ArrayList<>();
 
     public MultiAdapter(ArrayList<Player> data, Context context) {
         this.data = data;
-        this.context = context;
+            this.context = context;
+//            for(int i=0;i<data.size();i++){
+//                dataImg.add(new ImageView(context));
+//        }
     }
 
+    public ArrayList<ImageView> getDataImg() {
+        return dataImg;
+    }
 
     @Override
     public int getCount() {
@@ -50,6 +58,9 @@ public class MultiAdapter extends BaseAdapter {
         TextView txtScore = view.findViewById(R.id.txtScorePlayer);
         ImageView img = view.findViewById(R.id.imgPlayer);
         img.setImageResource(data.get(i).getAvatarId());
+
+        if(i==0) img.setBackgroundColor(Color.YELLOW);
+        dataImg.add(img);
         txtName.setText(data.get(i).getName());
         txtScore.setText(data.get(i).getScore()+"");
         return view;
